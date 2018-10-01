4f011229af60c3c9c61b34972909870a8aeb010c
Elyes Graba <elyesgraba@gatech.edu>
2018-09-30 19:11:21 -0500

    filled out the index.html and index.js with basic code to create a minimal DOM structure and then hook the root react component into that DOM structure

---
+++ src/client/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import App from './app/app';
+
+const reactRoot = document.getElementById('react-root');
+
+if (reactRoot) {
+    ReactDOM.render(App, reactRoot);
+} else {
+    console.warn("No DOM element to hook react app to.");
+}
