740643ce75d1ff6be4dd5695f0410cb4cd1e587f
Elyes Graba <elyesgraba@gatech.edu>
2018-09-30 20:49:17 -0500

    fixed react sytax goof in index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/app';
 
 const reactRoot = document.getElementById('react-root');
 
 if (reactRoot) {
-    ReactDOM.render(App, reactRoot);
+    ReactDOM.render(<App />, reactRoot);
 } else {
     console.warn("No DOM element to hook react app to.");
 }
