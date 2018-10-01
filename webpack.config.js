1a05ef73db1deafdd24b08cb222df8b8f5ff9cd7
Elyes Graba <elyesgraba@gatech.edu>
2018-09-30 20:48:14 -0500

    working webpack config, webpack dev server finally does what we want

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
+const htmlPlugin = new HtmlWebPackPlugin({
+    template: './src/client/index.html',
+    filename: './index.html'
+})
 
 module.exports = {
-    entry: './src/client',
-    mode: 'development',
+    entry: ['./src/client/index.js'],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
     module: {
-        rules: [
+          rules: [
             {
                 test: /\.jsx?$/,
-            },
+                resolve: {
+                    extensions: ['.js', '.jsx']
+                },
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }
         ]
-    }
+    },
+    plugins: [htmlPlugin]
 }
