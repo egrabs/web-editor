439984451fb1fd9e4cce3943e7da7f37051e9c89
Elyes Graba <elyesgraba@gatech.edu>
2018-09-30 19:10:51 -0500

    created an App react component to serve as the root component for the app

---
+++ src/client/app/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>
+                    Ok I guess this 'technically' does something now hehehehuhuh. . . 
+                </p>
+            </div>
+        );
+    }
+}
