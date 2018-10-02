453a348cc1cb55d4fbbdf1782ed978e4fd09b65a
Elyes Graba <elyesgraba@gatech.edu>
2018-10-01 20:24:10 -0500

    renamed index.js to index.jsx, cause, well it has jsx in it and jsx in a js file made eslint mad

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -7,5 +7,5 @@ const reactRoot = document.getElementById('react-root');
 if (reactRoot) {
     ReactDOM.render(<App />, reactRoot);
 } else {
-    console.warn("No DOM element to hook react app to.");
+    console.warn('No DOM element to hook react app to.');
 }
