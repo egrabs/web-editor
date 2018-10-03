2f684fe561cfca2b682761af0c9da80981aacc4a
Adrian Preston <prestaj0@sewanee.edu>
2018-10-03 14:10:19 -0500

    First draft of the AutoCompleteCache

--- src/client/utils/AutoCompleteCache.js
+++ src/client/utils/AutoCompleteCache.js
@@ -1,8 +1,49 @@
 /* eslint-disable */
 
 export function registerKeyStroke(text) {
-    /* Aj does this */
+    var prefixeCache = new PrefixStruct(text);
+    return
 }
+
+function AlphaNode(values=[]){
+    this.words = values;
+    this.links = new Map();
+}
+
+function PrefixStruct(words){
+    const alphaNumerics = 'abcdefghijklmnopqrstuvwxyz';
+    this.root = new AlphaNode(words);
+
+    this.populateStruct = function populate(node, s_idx){
+        for (let chr of alphaNumerics){
+            var wordGroup = node.words.filter(function(s){
+                return ((s.length > s_idx) && (s.charAt(s_idx) === chr))
+            });
+            if (wordGroup === []) continue;
+
+            node.links.set(chr, new AlphaNode(wordGroup));  
+            populate(node.links.get(chr), s_idx+1);
+        }
+    }
+    this.populateStruct(this.root, 0);
+
+    this.searchPrefix = function(prefix){
+        var subTreeRoot = this.root;
+        for (let chr of prefix){
+            if (!(subTreeRoot.links.keys().includes(chr))){
+                return null;
+            }
+            subTreeRoot = subTreeRoot.links.get(chr)
+        }
+        return subTreeRoot.words;
+    }
+}
+
+
+
+
+
+
 
 // function MyObjConstructor(param1, param2) {
 //     this.param1 = param1;
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,5 +3,5 @@ const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'eval-source-map',
+    devtool: 'eval-source-map'
 });
