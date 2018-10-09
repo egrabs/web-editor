c8fa3ce2d247cf147645c4375f41c09866fbb3b7
Elyes Graba <elyesgraba@gatech.edu>
2018-10-08 20:27:56 -0500

    commit a not broken CodeEditor.jsx, added import back in

--- src/client/components/CodeEditor/CodeEditor.jsx
+++ src/client/components/CodeEditor/CodeEditor.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Controlled as CodeMirror } from 'react-codemirror2';
 
-// import { registerKeyStroke } from '../../utils/AutoCompleteCache';
+import { registerKeyStroke } from '../../utils/AutoCompleteCache';
 import { startExecutionAnimation, stopExecutionAnimation } from '../../redux/RootActions';
 
 import styles from './CodeEditor.scss';
