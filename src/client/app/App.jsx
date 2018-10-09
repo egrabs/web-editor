1962da1e167b17f676207db600f58f1f2e8807ee
Elyes Graba <elyesgraba@gatech.edu>
2018-10-09 09:58:56 -0500

    hopefully fixed this stupid file casing issue once and for all

---
+++ src/client/app/App.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducer from '../redux/RootReducer';
+import AppContainer from './AppContainer';
+
+/* eslint-disable no-underscore-dangle */
+const rootStore = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Provider store={rootStore}>
+                <AppContainer />
+            </Provider>
+        );
+    }
+}
