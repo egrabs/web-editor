fb5f4733dd08b1a3c6b8ea1343c516134f6a3e18
Adrian Preston <prestaj0@sewanee.edu>
2018-10-08 20:51:56 -0500

    She works again

--- src/client/utils/AutoCompleteCache.js
+++ src/client/utils/AutoCompleteCache.js
@@ -1,115 +1,89 @@
-/* eslint-disable */
-
-var   root;
-const delimiters    = ' &|!?()[]{},.+-*%/=';
-const alphaNumerics = 'abcdefghijklmnopqrstuvwxyz'+
-                      'ABCDEFGHIJKLMNOPQRSTUVWXYZ'+
-                      '0123456789_';
+let root;
+const delimiters = ' &|!?()[]{},.+-*%/=';
+const alphaNumerics = 'abcdefghijklmnopqrstuvwxyz'
+                      + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+                      + '0123456789_';
 
 /*
 Node object we leverage to maintain a persistent and quickly
-searchable cache containing all words Client has typed so far. 
+searchable cache containing all words Client has typed so far.
 'words' is a set of single-word strings.
 'links' is a map w/ single-char keys mapped to AlphaNode objects
 */
-function AlphaNode(values=new Set([])){
+function AlphaNode(values = new Set([])) {
     this.words = values;
     this.links = new Map();
 }
 
-
 /*
-This function handles all possible responses to any given
-keystroke, parsing when appropriate and handing off work to 
-a subroutine: cacheWord(), searchPrefix(), or cleanUp().
-*/ 
-export function registerKeyStroke(word) {
-    cacheWord(word);
-    return; 
-}
-
-/* 
-Called on space key press, w/ last word typed by client as argument;
-Modifies the persistent cache of used words 
+- Called on space key press, w/ last word typed by client as argument;
+- Modifies the persistent cache of used words
 */
-function cacheWord(word){
+function cacheHelper(word) {
+    let node = root;
+    let sIdx = 0;
+    node.words.add(word);
+    while (sIdx < word.length) {
+        const nextLinks = Array.from(node.links.keys());
+        const chr = word.charAt(sIdx);
 
+        if (!nextLinks.includes(chr)) {
+            node.links.set(chr, new AlphaNode());
+        }
+
+        node = node.links.get(chr);
+        node.words.add(word);
+        sIdx += 1;
+    }
+}
+function cacheWord(word) {
     if (root === undefined) root = new AlphaNode();
     else if (root.words.has(word)) return;
     cacheHelper(root, word, 0);
-    return
-}
-function cacheHelper(node, word, sIdx){
-    while (sIdx < word.length){
-        var nextLinks = Array.from(node.links.keys());
-        var chr = word.charAt(sIdx)
-
-        node.words.add(word);
-
-        if (!nextLinks.includes(chr)){
-            node.links.set(chr, new AlphaNode());
-        }
-        node = node.links.get(chr)
-    }
-    return 
 }
 
 /*
--Called on any non-space, non-delimiter key press with, 
+Handles all possible responses to any given
+keystroke, parsing when appropriate and handing off work to
+a subroutine: cacheWord(), searchPrefix(), or cleanUp().
+*/
+export function registerKeyStroke(word) {
+    cacheWord(word);
+}
+
+/*
+- Called on any non-space, non-delimiter key press, with
 the prefix currently being typed by client as argument;
--Does not modify the persistent cache;
+- Does not modify the persistent cache;
 Performs a very fast search for the prefix and returns
 an array of completion suggestions
 */
-function searchPrefix(prefix){
-    var subTreeRoot = this.root;
-    for (let chr of prefix){
-        var nextLinks = Array.from(subTreeRoot.links.keys())
+function searchPrefix(prefix) {
+    let node = root;
+    for (const chr of prefix) {
+        const nextLinks = Array.from(node.links.keys());
         if (!nextLinks.includes(chr)) return null;
-        subTreeRoot = subTreeRoot.links.get(chr)
+        node = node.links.get(chr);
     }
-    return subTreeRoot.words;
-}
-
-function parseFileText(text){
-    var parsedText = [text];
-
-    for (let delim of delimiters){
-        parsedText = parsedText.map(function(s){
-            return s.split(delim)
-        })
-        parsedText = [].concat.apply([], parsedText);
-    }
-
-    parsedText = parsedText.filter(function(s){
-        return (s != '' && s != "");
-    })
-    return parsedText;
+    return node.words;
 }
 
 /*
-function PrefixStruct(words){
-    this.populateStruct = function populate(node, s_idx){
-    this.root = new AlphaNode(words);
-    populateStruct(this.root, 0);
-}
-const alphaNumerics = 'abcdefghijklmnopqrstuvwxyz'+
-                      'ABCDEFGHIJKLMNOPQ
-
-function populateStruct(node, s_idx){
-    const alphaNumerics = 'abcdefghijklmnopqrstuvwxyz';
-
-    for (let chr of alphaNumerics){
-
-        var wordGroup = node.words.filter(function(s){
-            return (s.length > s_idx)&&(s.charAt(s_idx) === chr)
-        });
-        if (wordGroup.length === 0) continue;
-
-        node.links.set(chr, new AlphaNode(wordGroup));  
-        populateStruct(node.links.get(chr), s_idx+1);
-    }
-}
+Repeatedly tokenizes the input string on any of the specified 
+delimeters, flattening the results into a 1-d array of words
 */
+function parseFileText(text) {
+    let parsedText = [text];
 
+    for (const delim of delimiters) {
+        parsedText = parsedText.map(function (s) {
+            return s.split(delim);
+        });
+        parsedText = [].concat.apply([], parsedText);
+    }
 
+    parsedText = parsedText.filter(function (s) {
+        return (s !== '' && s !== "");
+    });
+    return parsedText;
+}
