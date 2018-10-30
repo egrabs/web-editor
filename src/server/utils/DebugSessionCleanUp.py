66a66c14de682e6cae597a999c7c6335c0e24d29
Elyes Graba <elyesgraba@gatech.edu>
2018-10-29 21:06:32 -0500

    restore DebugSessionCleanup constants to their previous values

--- src/server/utils/DebugSessionCleanUp.py
+++ src/server/utils/DebugSessionCleanUp.py
@@ -5,10 +5,15 @@ from utils.DebugSessionCache import getAllSessionIds, _cache, isSessionExpired
 
 import threading as thrd
 
-PROCESS_EXPIRY_LIMIT = 10  # 10 mins
+PROCESS_EXPIRY_LIMIT = 600  # 10 mins
 
-RUN_EVERY = 10  # 5 mins
+RUN_EVERY = 300  # 5 mins
 
+# could this cause a race condition?
+# if _cleanSessions has only partially executed when the OS decides to
+# let a different thread run for a bit, could a user request to /debugAction
+# end up grabbing a cached session that's been halfway cleaned?
+# maybe its processes have been terminated but it hasn't set expired=True yet?
 def _cleanSessions(cache):
     while True:
         time.sleep(RUN_EVERY)
@@ -19,7 +24,6 @@ def _cleanSessions(cache):
             session = cache[seshId]
             lastInteraction = session['lastInteraction']
             if time.time() - lastInteraction >= PROCESS_EXPIRY_LIMIT:
-                print 'its happening!'
                 session['proc'].terminate()
                 session['accumThread'].terminate()
                 fname = session['filename']
