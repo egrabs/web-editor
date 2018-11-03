bbd0d70544f91d5fcf107f9314f832eeb6c14326
Camila Martinica <Camila@Camilas-MacBook-Air.local>
2018-11-02 20:00:55 -0500

    got the ast tree viz in a good state

--- src/client/components/ASTTree/ASTTreeController.js
+++ src/client/components/ASTTree/ASTTreeController.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-/* eslint-disable no-param-reassign, no-debugger, no-confusing-arrow, no-unused-vars, no-underscore-dangle */
+/* eslint-disable no-param-reassign, no-debugger, no-confusing-arrow, no-underscore-dangle */
 import * as d3 from 'd3';
 
 /**
@@ -16,10 +16,10 @@ function processAst(ast) {
 }
 
 const margin = {
+    left: 10,
+    right: 10,
     top: 10,
-    right: 120,
     bottom: 10,
-    left: 40,
 };
 
 const diagonal = d3.linkVertical().x(d => d.x).y(d => d.y);
@@ -30,74 +30,47 @@ export default function ASTTreeController(props) {
 
     const transformed = processAst(ast);
 
-    console.log(transformed);
-
     const root = d3.hierarchy(transformed, d => d.children);
 
     const { width, height } = svg.node().getBoundingClientRect();
 
-    const effectiveWidth = width - 20;
-    const effectiveHeight = height - 20;
+    const effectiveWidth = width - margin.left - margin.right;
+    const effectiveHeight = height - margin.top - margin.bottom;
 
     const tree = d3.tree().size([effectiveWidth, effectiveHeight]);
 
     root.x = width / 2;
-    root.y = 10;
+    root.y = 0;
     root.descendants().forEach((d, i) => {
         d.id = i;
-        // d._children = d.children;
-        // if (d.depth && d.data._type.length !== 7) d.children = null;
     });
 
-    svg // .attr('viewBox', [-margin.left, -margin.top, width, height])
-        .style('font', '10px sans-serif')
+    svg.style('font', '10px sans-serif')
         .style('user-select', 'none');
 
+    const gNode = svg.append('g')
+        .attr('transform', `translate(0,${margin.top})`);
+
     const gLink = svg.append('g')
+        .attr('transform', `translate(0,${margin.bottom})`)
         .attr('fill', 'none')
         .attr('stroke', '#555')
         .attr('stroke-opacity', 0.4)
         .attr('stroke-width', 1.5);
 
-    const gNode = svg.append('g');
-    // .attr('cursor', 'pointer');
-
-    function update(source) {
-        const duration = d3.event && d3.event.altKey ? 2500 : 250;
-        const nodes = root.descendants().reverse();
+    function update() {
+        const nodes = root.descendants();
         const links = root.links();
 
-        // Compute the new tree layout.
         tree(root);
 
-        let left = root;
-        let right = root;
-        root.eachBefore((node) => {
-            if (node.x < left.x) left = node;
-            if (node.x > right.x) right = node;
-        });
-
-        // const height = right.x - left.x + margin.top + margin.bottom;
-
-        const transition = svg.transition()
-            .duration(duration)
-            // .attr('height', height)
-            // .attr('viewBox', [-margin.left, left.x - margin.top, width, height])
-            .tween('resize', window.ResizeObserver ? null : () => () => svg.dispatch('toggle'));
-
-        // Update the nodes…
         const node = gNode.selectAll('g')
             .data(nodes, d => d.id);
 
-        // Enter any new nodes at the parent's previous position.
         const nodeEnter = node.enter().append('g')
             .attr('transform', d => `translate(${d.x},${d.y})`)
-            .attr('fill-opacity', 0)
-            .attr('stroke-opacity', 0);
-            // .on('click', (d) => {
-            //     d.children = d.children ? null : d._children;
-            //     update(d);
-            // });
+            .attr('fill-opacity', 1)
+            .attr('stroke-opacity', 1);
 
         nodeEnter.append('circle')
             .attr('r', 2.5)
@@ -105,57 +78,16 @@ export default function ASTTreeController(props) {
 
         nodeEnter.append('text')
             .attr('dy', '0.31em')
-            .attr('x', d => d._children ? -6 : 6)
-            .attr('text-anchor', d => d._children ? 'end' : 'start')
-            .text(d => d.data._type)
-            .clone(true)
-            .lower()
-            .attr('stroke-linejoin', 'round')
-            .attr('stroke-width', 3)
-            .attr('stroke', 'white');
+            .attr('x', 6)
+            .attr('text-anchor', 'start')
+            .text(d => d.data._type);
 
-        // Transition nodes to their new position.
-        const nodeUpdate = node.merge(nodeEnter).transition(transition)
-            .attr('transform', d => `translate(${d.x},${d.y})`)
-            .attr('fill-opacity', 1)
-            .attr('stroke-opacity', 1);
-
-        // Transition exiting nodes to the parent's new position.
-        // const nodeExit = node.exit().transition(transition).remove()
-        //     .attr('transform', d => `translate(${source.x},${source.y})`)
-        //     .attr('fill-opacity', 0)
-        //     .attr('stroke-opacity', 0);
-
-        // Update the links…
         const link = gLink.selectAll('path')
             .data(links, d => d.target.id);
 
-        // Enter any new links at the parent's previous position.
-        const linkEnter = link.enter().append('path')
+        link.enter().append('path')
             .attr('d', diagonal);
-            // .attr('d', (d) => {
-            //     const o = { x: source.x0, y: source.y0 };
-            //     return diagonal({ source: o, target: o });
-            // });
-
-        // Transition links to their new position.
-        // link.merge(linkEnter).transition(transition)
-
-        // Transition exiting nodes to the parent's new position.
-        // link.exit().transition(transition).remove()
-        //     .attr('d', (d) => {
-        //         const o = { x: source.x, y: source.y };
-        //         return diagonal({ source: o, target: o });
-        //     });
-
-        // Stash the old positions for transition.
-        root.eachBefore((d) => {
-            d.x0 = d.x;
-            d.y0 = d.y;
-        });
     }
 
     update(root);
-
-    return svg.node();
 }
