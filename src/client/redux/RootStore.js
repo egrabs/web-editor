cda73e11d1ff5b626ec3163bcf642fb6f430074c
Elyes Graba <egrabs@Elyess-MacBook-Pro.local>
2018-12-08 21:40:04 -0600

    add redux-thunk middleware to root reducer

--- src/client/redux/RootStore.js
+++ src/client/redux/RootStore.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import rootReducer from './RootReducer';
 
@@ -6,6 +7,7 @@ import rootReducer from './RootReducer';
 const rootStore = createStore(
     rootReducer,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk),
 );
 
 export default rootStore;
